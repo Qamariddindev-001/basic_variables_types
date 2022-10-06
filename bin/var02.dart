ca779fb604d8183d747a918ff56bc82ecbba5fd0
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-06 15:57:33 +0500

    var02.dart

--- bin/var02.dart
+++ bin/var02.dart
@@ -7,5 +7,11 @@ Create a variable of type int  and assign your year of birth
 */
 
 void main() {
+  int a = 2000;
+  int b = 18;
+  int c = 11;
+  print(a);
+  print(b);
+  print(c);
   // write your code here
 }

264948f814db33d6a12ea6c90990dee8de9f55fa
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-06 16:27:17 +0500

    var08.dart

--- bin/var08.dart
+++ bin/var08.dart
@@ -5,5 +5,19 @@ Create a string  variable that contains  letters and numbers
 */
 
 void main() {
+  int a1, a2, a3, a4, a5, a6, a7, a8, a9, a10;
+  a1 = 4;
+  a2 = 5;
+  a3 = 8;
+  a4 = 5;
+  a5 = 9;
+  a6 = 10;
+  a7 = 22;
+  a8 = 33;
+  a9 = 99;
+  a10 = 100;
+  int summ = a1 * a10 * a2 - a3 + a4 ~/ a5 + a9 ~/ a8 ~/ a7 ~/ a6;
+  print(summ);
+
   // write your code here
 }

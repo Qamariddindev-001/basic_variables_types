153a4261ab1665762186a5d65958d33c43d955a7
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-06 15:52:44 +0500

    var01.dart

--- bin/var01.dart
+++ bin/var01.dart
@@ -6,5 +6,13 @@ Create a variable of type int and assign it any value
 */
 
 void main() {
+  int a;
+  int b;
+  int c;
+  a = 4;
+  b = 5;
+  c = 18;
+  int sum = a * c - b;
+  print(sum);
   // write your code here
 }

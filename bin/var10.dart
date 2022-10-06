42af7ae4b5f9702383eff12a6b9ad1d71838dc86
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-06 16:40:14 +0500

    var10.dart

--- bin/var10.dart
+++ bin/var10.dart
@@ -9,5 +9,9 @@ print your last name, then print your first name at the bottom.
 */
 
 void main() {
+  String fristName = 'Qamariddin';
+  String lastName = 'Oltiboyev';
+  print(lastName);
+  print(fristName);
   // write your code here
 }
